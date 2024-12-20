ea862aa58ef64b05dc3de629e6df4a97ce83c96b
Eric Zhang <superezzhang@gmail.com>
2024-12-19 19:28:45 -0500

    Add To-Do item addition functionality

---
+++ src/script.js
@@ -0,0 +1,21 @@
+
+
+const createToDo = document.getElementById('create-to-do');
+const toDoList = document.getElementById('to-do-list');
+createToDo.addEventListener('click', function(){
+  let toDoInput = document.getElementById('input-bar');
+  const toDoText = toDoInput.value;
+  if (toDoText === ""){ // Edge case check for empty to-do
+    console.log("Error: Empty To-Do");
+    return;
+  }
+  // Create a new to-do and append it to the to do list 
+  const toDo = document.createElement('li');
+  toDo.setAttribute('class', 'to-do');
+  toDo.textContent = toDoText;
+  toDoList.appendChild(toDo);
+  // Reset Search Bar
+  toDoInput.value = "";
+  return;
+});
+
