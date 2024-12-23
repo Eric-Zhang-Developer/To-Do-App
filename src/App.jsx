21cdeed95f90c94703ee9bfe3f9a97e715c1b259
Eric Zhang <superezzhang@gmail.com>
2024-12-22 21:53:33 -0500

    Add Title

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <h1 className='title'>To-Do List</h1>
     </>
   )
 }
